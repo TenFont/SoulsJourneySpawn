040194a8b1a94b2e6a72cf3b32360c95c4229fce
TenFont <thetenfont@gmail.com>
2023-10-01 12:00:07 +0500

    Disable interacting with blocks at spawn

--- src/main/java/org/tenfont/soulsjourneyspawn/events/DisableBuild.java
+++ src/main/java/org/tenfont/soulsjourneyspawn/events/DisableBuild.java
@@ -1,9 +1,11 @@
 package org.tenfont.soulsjourneyspawn.events;
 
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.tenfont.soulsjourneyspawn.util.RegionUtils;
 
 public class DisableBuild implements Listener {
@@ -16,6 +18,14 @@ public class DisableBuild implements Listener {
     @EventHandler
     public void onBreak(BlockBreakEvent event) {
         if (RegionUtils.isLocationInsideSpawn(event.getBlock().getLocation()) && !event.getPlayer().hasPermission("soulsjourneyspawn.admin"))
+            event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        Block block = event.getClickedBlock();
+        if (block == null) return;
+        if (RegionUtils.isLocationInsideSpawn(block.getLocation()) && !event.getPlayer().hasPermission("soulsjourneyspawn.admin"))
             event.setCancelled(true);
     }
 }
